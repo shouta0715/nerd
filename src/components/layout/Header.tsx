dc868a518da2396bb743d11713e5007893b9015a
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-03 19:56:02 +0900

    change:text-size

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,11 @@ export const Header: FC = () => (
   <header className="fixed top-0 h-16  w-full border-b border-l-0 border-t-0 border-r-0 border-solid border-gray-200   shadow-sm md:border-none">
     <div className="container mx-auto h-full border-b border-l-0 border-t-0 border-r-0 border-none border-gray-200 md:border-solid">
       <form className="flex h-full items-center justify-center space-x-4">
-        <Autocomplete className="w-64" placeholder="検索する" data={["test"]} />
+        <Autocomplete
+          className="w-64 text-base"
+          placeholder="検索する"
+          data={["test"]}
+        />
         <Button>検索</Button>
       </form>
     </div>
