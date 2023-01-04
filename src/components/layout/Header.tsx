e83f8bc7067cadb13a786fab49f2896769af72ec
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-04 13:24:56 +0900

    shadow

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,12 +3,13 @@ import { Autocomplete, ThemeIcon } from "@mantine/core";
 import { IconAdjustmentsHorizontal, IconSearch } from "@tabler/icons";
 
 export const Header: FC = () => (
-  <header className="md:shadow-non fixed top-0  h-16 w-full border-b border-l-0 border-t-0 border-r-0 border-solid  border-gray-200 shadow-sm md:border-none">
+  <header className="md:shadow-non fixed top-0  h-16 w-full border-b border-l-0 border-t-0 border-r-0 border-solid  border-gray-200  md:border-none">
     <div className="e container mx-auto h-full ">
-      <div className=" flex h-full items-center justify-center border-b border-l-0 border-t-0 border-r-0 border-none border-gray-200 md:border-solid md:pl-[10%] lg:pr-[33.3333%]">
+      <div className=" flex h-full items-center justify-center border-b border-l-0 border-t-0 border-r-0 border-none border-gray-200 shadow-sm md:ml-[10%] md:border-solid lg:mr-[33.3333%]">
         <form className="flex h-full items-center justify-center space-x-4">
           <Autocomplete
             className=" w-72 text-2xl"
+            color="green"
             placeholder="キーワードで検索"
             data={["test"]}
             size="md"
