516309c23f513f682a30ee2f5f3a3aaf40b67de3
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-04 09:32:41 +0900

    foooter sizeの縮小

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,16 +12,16 @@ export const Footer: FC = () => {
   );
 
   return (
-    <footer className="fixed bottom-0 h-20 w-full border-x-0 border-t border-b-0 border-solid border-gray-200  bg-white shadow-md md:hidden">
+    <footer className="fixed bottom-0 h-14 w-full border-x-0 border-t border-b-0 border-solid border-gray-200  bg-white shadow-md md:hidden">
       <Flex justify="space-around" align="center" className="h-full w-full">
-        <Burger opened={isOpenBurger} size={36} onClick={changeIsOpenBurger} />
-        <HomeIcon className="h-10 w-10 cursor-pointer stroke-[0.5px] transition-all active:scale-90" />
-        <ThemeIcon className=" h-14 w-14 rounded-full transition-all active:scale-90">
-          <PlusIcon className="h-10 w-10 stroke-[1.5px]" />
+        <Burger opened={isOpenBurger} size={24} onClick={changeIsOpenBurger} />
+        <HomeIcon className="h-6 w-6 cursor-pointer stroke-[0.5px] transition-all active:scale-90" />
+        <ThemeIcon className=" h-10 w-10 rounded-full transition-all active:scale-90">
+          <PlusIcon className="h-8 w-8 stroke-[1px]" />
         </ThemeIcon>
-        <BellIcon className="h-10 w-10 cursor-pointer stroke-[0.5px] transition-all active:scale-90" />
+        <BellIcon className="h-6 w-6 cursor-pointer stroke-[0.5px] transition-all active:scale-90" />
         {true ? (
-          <UserCircleIcon className="h-10 w-10 cursor-pointer rounded-full text-gray-500 transition-all active:scale-90" />
+          <UserCircleIcon className="h-6 w-6 cursor-pointer rounded-full text-gray-500 transition-all active:scale-90" />
         ) : (
           <Avatar />
         )}
