e25a91a1937f611546bf90a2e2d8729a40908fde
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-05 19:39:49 +0900

    リファクタリング

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -3,6 +3,7 @@ import { useQueryPosts } from "../../hooks/posts/useQueryPosts";
 
 export const Post: FC = () => {
   const { data } = useQueryPosts();
+  console.log(data);
 
   return <div>Post</div>;
 };
