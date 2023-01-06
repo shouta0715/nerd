37a642d13f04b43092aabfdff07bd206905ab1f1
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-06 15:23:44 +0900

    googleログイン

--- src/components/atom/GoogleSignIn.tsx
+++ src/components/atom/GoogleSignIn.tsx
@@ -1,15 +1,23 @@
 import { Text } from "@mantine/core";
 import Image from "next/image";
 import React, { FC } from "react";
+import { useGoogleSignIn } from "../../hooks/auth/useGoogleSignIn";
 
-export const GoogleSignIn: FC = () => (
-  <figure className="mx-auto flex w-[250px] cursor-pointer items-center justify-center space-x-4 rounded-full border border-solid border-gray-400 py-1 px-6">
-    <Image
-      className="!relative !h-8 !w-8	object-cover"
-      src="/Google.png"
-      alt=""
-      fill
-    />
-    <Text fw="bold">SignIn With Google</Text>
-  </figure>
-);
+export const GoogleSignIn: FC = () => {
+  const signInGoogle = useGoogleSignIn();
+
+  return (
+    <button
+      onClick={signInGoogle}
+      className="mx-auto  flex w-[250px] cursor-pointer items-center justify-center space-x-4 rounded-full border border-solid border-gray-400 bg-white py-1 px-6"
+    >
+      <Image
+        className="!relative !h-8 !w-8	object-cover"
+        src="/Google.png"
+        alt=""
+        fill
+      />
+      <Text fw="bold">SignIn With Google</Text>
+    </button>
+  );
+};
