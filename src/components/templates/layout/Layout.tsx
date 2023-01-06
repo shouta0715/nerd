86d5058634e49a96681cb6cffa555663b83fe086
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-06 22:31:33 +0900

    zindex

--- src/components/templates/layout/Layout.tsx
+++ src/components/templates/layout/Layout.tsx
@@ -12,9 +12,9 @@ type Props = {
 export const Layout: FC<Props> = ({ children }) => (
   <div className="min-w-screen min-h-screen">
     <Header />
-    <main className=" container mx-auto flex h-full w-full border-l-0 border-r border-b-0 border-t-0 border-solid border-gray-200">
+    <main className=" container z-0 mx-auto flex h-full w-full border-l-0 border-r border-b-0 border-t-0 border-solid border-gray-200">
       <Nav />
-      <ScrollArea className="mx-auto mt-16 w-full flex-1 ">
+      <ScrollArea className="mx-auto mt-16 mb-14 w-full flex-1 md:mb-0 ">
         {children}
       </ScrollArea>
       <Aside />
