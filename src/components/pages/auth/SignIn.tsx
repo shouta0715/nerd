b6788798374a14ae0db6c3fbaca7476f8d06dba7
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-06 16:23:12 +0900

    layoutの調整

--- src/components/pages/auth/SignIn.tsx
+++ src/components/pages/auth/SignIn.tsx
@@ -1,15 +1,28 @@
-import { ScrollArea, Title } from "@mantine/core";
+import { CloseButton, ScrollArea, Title } from "@mantine/core";
+import Link from "next/link";
 import React, { FC } from "react";
 import { GoogleSignIn } from "../../atom/GoogleSignIn";
 import { MailWithPassword } from "../../organism/MailWithPassword";
 
 export const SignIn: FC = () => (
-  <ScrollArea className="h-full max-h-full w-full max-w-full rounded-lg bg-white md:h-[800px]  md:w-[700px] md:shadow-2xl">
-    <div className="flex h-screen w-screen flex-col items-center justify-center  p-6 md:h-full md:w-full md:p-10">
+  <ScrollArea className="h-screen max-h-full w-screen max-w-full rounded-lg bg-white md:h-[800px]  md:w-[700px] md:shadow-2xl">
+    <div className="relative flex h-full min-h-screen w-screen flex-col items-center  justify-center  p-6 md:h-[800px] md:min-h-0  md:w-[700px] md:p-10">
+      <Link
+        href="/"
+        className="relative -left-[calc(50%-40px)] rounded-full md:absolute  md:left-10  md:top-10"
+      >
+        <CloseButton
+          aria-label="Close modal"
+          size="xl"
+          color="green"
+          radius="xl"
+          variant="light"
+        />
+      </Link>
       <Title order={1}>Login</Title>
       <div className="mt-10">
         <GoogleSignIn />
-        <p className="my-10 flex w-full items-center justify-center text-center  text-indigo-500 before:mr-4 before:inline-block before:h-[1px] before:w-[150px] before:bg-indigo-400 before:content-[''] after:ml-4 after:inline-block after:h-[1px] after:w-[150px] after:bg-indigo-400 after:content-['']  ">
+        <p className="my-10 flex w-full items-center justify-center text-center  text-sm text-indigo-500 before:mr-4 before:inline-block before:h-[1px] before:w-[150px] before:bg-indigo-400 before:content-[''] after:ml-4 after:inline-block after:h-[1px] after:w-[150px] after:bg-indigo-400 after:content-[''] md:text-base  ">
           または
         </p>
         <form className="flex flex-col items-center justify-center space-y-8  px-6">
