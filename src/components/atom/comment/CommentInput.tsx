4e1391d8683debc9f2f8d68337c4e8ccf25c99ae
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-09 01:18:53 +0900

    inputのみfixedに変更

--- src/components/atom/comment/CommentInput.tsx
+++ src/components/atom/comment/CommentInput.tsx
@@ -12,7 +12,7 @@ export const CommentInput: FC = () => {
   const match = useMediaQuery("(min-width: 768px)");
 
   return (
-    <div className="sticky bottom-0 h-[70px] w-full border-x-0 border-b-0 border-t border-solid border-gray-200 py-2 md:static md:h-auto md:py-6">
+    <div className="fixed bottom-14 h-[70px] w-full border-x-0 border-b-0 border-t border-solid border-gray-200 py-2 md:static md:h-auto md:py-6">
       <div className="flex items-center md:flex-col md:items-start  md:space-y-4">
         <div className="flex flex-col items-center pl-6 md:flex-row md:space-x-4">
           <Avatar
