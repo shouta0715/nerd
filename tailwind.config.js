fb1de41e8fea00ad914e2f8745690e49cea93f0e
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-09 00:01:37 +0900

    safari

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        "screen": "100dvh",
+      }
+    },
   },
   plugins: [],
   corePlugins: {
