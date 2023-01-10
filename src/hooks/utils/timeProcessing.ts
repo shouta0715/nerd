0219be4814af8217b756c0e59d324f7f18dcbcf7
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-10 22:19:54 +0900

    change:comment asc

--- src/hooks/utils/timeProcessing.ts
+++ src/hooks/utils/timeProcessing.ts
@@ -17,5 +17,24 @@ export const timeProcessing = () => {
     seconds: number;
   }) => hours * 3600 + minutes * 60 + seconds;
 
-  return { secondToTime, timeToSecond };
+  const timeCommented = (second: number): string => {
+    let commented = "";
+    const { hours, minutes, seconds } = secondToTime(second);
+
+    if (hours > 0) {
+      commented += `${hours}時間 `;
+    }
+
+    if (minutes > 0) {
+      commented += `${minutes}分 `;
+    }
+
+    if (seconds >= 0) {
+      commented += `${seconds}秒`;
+    }
+
+    return commented;
+  };
+
+  return { secondToTime, timeToSecond, timeCommented };
 };
