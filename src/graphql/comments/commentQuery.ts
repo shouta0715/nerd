7303036b02bbaf6b4de831d0baf4477daea41606
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-11 14:22:33 +0900

    comment query にorder_byの追加

--- src/graphql/comments/commentQuery.ts
+++ src/graphql/comments/commentQuery.ts
@@ -2,7 +2,10 @@ import { gql } from "graphql-request";
 
 export const GET_COMMENTS = gql`
   query GetComments($post_id: uuid!) {
-    comments(order_by: { time: asc }, where: { post_id: { _eq: $post_id } }) {
+    comments(
+      order_by: { time: asc, created_at: asc }
+      where: { post_id: { _eq: $post_id } }
+    ) {
       user_id
       time
       spoiler
