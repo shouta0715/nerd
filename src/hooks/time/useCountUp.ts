2dabec1c0ef331923839d3acec36bc16e90e153a
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-17 14:15:58 +0900

    fix issue#38

--- src/hooks/time/useCountUp.ts
+++ src/hooks/time/useCountUp.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useInterval } from "@mantine/hooks";
 import { useEffect } from "react";
 import { useCommentTimeStore } from "../../store/comment/commentType";
@@ -8,15 +9,15 @@ export const useCountUp = () => {
   const time = useCommentTimeStore((state) => state.time);
   const setInterval = useGlobalTimerStore((state) => state.setInterval);
   const interval = useInterval(setTimer, 1000);
-  setInterval(interval);
+  useEffect(() => {
+    setInterval(interval);
+  }, [interval.active]);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => () => interval.stop(), []);
 
   return {
     minutes: time.minutes.toString().padStart(2, "0"),
     seconds: time.seconds.toString().padStart(2, "0"),
     hours: time.hours.toString().padStart(2, "0"),
-    interval,
   };
 };
