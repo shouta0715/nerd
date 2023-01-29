b54ed500b9f55f2f1a735c3624958560862a8a8e
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-01-29 19:27:24 +0900

    fix FOUC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,28 +3,32 @@ import { createGetInitialProps } from "@mantine/next";
 
 const getInitialProps = createGetInitialProps();
 
-export class _Document extends Documents {
+export default class _Document extends Documents {
   static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="常にリアルタイムで感想を共有しよう"
+          />
+          <meta property="og:site_name" content="Anime" />
+          <meta property="og:title" content="Anime" />
+          <meta
+            property="og:description"
+            content="常にリアルタイムで感想を共有しよう"
+          />
+        </Head>
+        <body>
+          <script> </script>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
-
-const Document = () => (
-  <Html lang="en">
-    <Head>
-      <meta charSet="utf-8" />
-      <link rel="icon" href="/favicon.ico" />
-      <meta name="description" content="常にリアルタイムで感想を共有しよう" />
-      <meta property="og:site_name" content="Anime" />
-      <meta property="og:title" content="Anime" />
-      <meta
-        property="og:description"
-        content="常にリアルタイムで感想を共有しよう"
-      />
-    </Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-);
-
-export default Document;
