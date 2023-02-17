ed72763cf08c51a5f27e272fe7344985e9e5d68f
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-02-17 21:01:35 +0900

    add @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 const nextConfig = {
   reactStrictMode: false,
   eslint: {
@@ -6,4 +9,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
