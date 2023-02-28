5dc9f08acdeb7f843a5051b9f094d6288f0d9d78
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-02-28 13:58:59 +0900

    add codegen

--- src/graphql/comment/commentQuery.generated.ts
+++ src/graphql/comment/commentQuery.generated.ts
@@ -1,37 +1,103 @@
-import * as Types from '../../types/graphql';
+import * as Types from "../../types/graphql";
 
-import { GraphQLClient } from 'graphql-request';
-import { RequestInit } from 'graphql-request/dist/types.dom';
-import { useQuery, useMutation, UseQueryOptions, UseMutationOptions } from '@tanstack/react-query';
+import { GraphQLClient } from "graphql-request";
+import { RequestInit } from "graphql-request/dist/types.dom";
+import {
+  useQuery,
+  useMutation,
+  UseQueryOptions,
+  UseMutationOptions,
+} from "@tanstack/react-query";
 
-function fetcher<TData, TVariables extends { [key: string]: any }>(client: GraphQLClient, query: string, variables?: TVariables, requestHeaders?: RequestInit['headers']) {
-  return async (): Promise<TData> => client.request({
-    document: query,
-    variables,
-    requestHeaders
-  });
+function fetcher<TData, TVariables extends { [key: string]: any }>(
+  client: GraphQLClient,
+  query: string,
+  variables?: TVariables,
+  requestHeaders?: RequestInit["headers"]
+) {
+  return async (): Promise<TData> =>
+    client.request({
+      document: query,
+      variables,
+      requestHeaders,
+    });
 }
 export type GetChatCommentsQueryVariables = Types.Exact<{
-  episode_id: Types.Scalars['uuid'];
+  episode_id: Types.Scalars["uuid"];
 }>;
 
-
-export type GetChatCommentsQuery = { __typename?: 'query_root', chat_comments: Array<{ __typename?: 'chat_comments', content: string, anonymous: boolean, created_at: any, episode_id?: any | null, id: any, time: number, commenter_name: string, user: { __typename?: 'users', anonymous: boolean, user_name: string, photo_url?: string | null, id: string } }> };
+export type GetChatCommentsQuery = {
+  __typename?: "query_root";
+  chat_comments: Array<{
+    __typename?: "chat_comments";
+    content: string;
+    anonymous: boolean;
+    created_at: any;
+    episode_id?: any | null;
+    id: any;
+    time: number;
+    commenter_name: string;
+    user: {
+      __typename?: "users";
+      anonymous: boolean;
+      user_name: string;
+      photo_url?: string | null;
+      id: string;
+    };
+  }>;
+};
 
 export type InsertChatCommentMutationVariables = Types.Exact<{
   object: Types.Chat_Comments_Insert_Input;
 }>;
 
-
-export type InsertChatCommentMutation = { __typename?: 'mutation_root', insert_chat_comments_one?: { __typename?: 'chat_comments', content: string, work_id?: number | null, user_id: string, time: number, id: any, episode_id?: any | null, created_at: any, commenter_name: string, user: { __typename?: 'users', anonymous: boolean, user_name: string, photo_url?: string | null, id: string } } | null };
+export type InsertChatCommentMutation = {
+  __typename?: "mutation_root";
+  insert_chat_comments_one?: {
+    __typename?: "chat_comments";
+    content: string;
+    work_id?: number | null;
+    user_id: string;
+    time: number;
+    id: any;
+    episode_id?: any | null;
+    created_at: any;
+    commenter_name: string;
+    user: {
+      __typename?: "users";
+      anonymous: boolean;
+      user_name: string;
+      photo_url?: string | null;
+      id: string;
+    };
+  } | null;
+};
 
 export type SubscriptionChatCommentsSubscriptionVariables = Types.Exact<{
-  episode_id: Types.Scalars['uuid'];
+  episode_id: Types.Scalars["uuid"];
 }>;
 
-
-export type SubscriptionChatCommentsSubscription = { __typename?: 'subscription_root', chat_comments: Array<{ __typename?: 'chat_comments', content: string, work_id?: number | null, user_id: string, time: number, id: any, episode_id?: any | null, created_at: any, commenter_name: string, user: { __typename?: 'users', anonymous: boolean, user_name: string, photo_url?: string | null, id: string } }> };
-
+export type SubscriptionChatCommentsSubscription = {
+  __typename?: "subscription_root";
+  chat_comments: Array<{
+    __typename?: "chat_comments";
+    content: string;
+    work_id?: number | null;
+    user_id: string;
+    time: number;
+    id: any;
+    episode_id?: any | null;
+    created_at: any;
+    commenter_name: string;
+    user: {
+      __typename?: "users";
+      anonymous: boolean;
+      user_name: string;
+      photo_url?: string | null;
+      id: string;
+    };
+  }>;
+};
 
 export const GetChatCommentsDocument = `
     query GetChatComments($episode_id: uuid!) {
@@ -56,24 +122,40 @@ export const GetChatCommentsDocument = `
 }
     `;
 export const useGetChatCommentsQuery = <
-      TData = GetChatCommentsQuery,
-      TError = unknown
-    >(
-      client: GraphQLClient,
-      variables: GetChatCommentsQueryVariables,
-      options?: UseQueryOptions<GetChatCommentsQuery, TError, TData>,
-      headers?: RequestInit['headers']
-    ) =>
-    useQuery<GetChatCommentsQuery, TError, TData>(
-      ['GetChatComments', variables],
-      fetcher<GetChatCommentsQuery, GetChatCommentsQueryVariables>(client, GetChatCommentsDocument, variables, headers),
-      options
-    );
+  TData = GetChatCommentsQuery,
+  TError = unknown
+>(
+  client: GraphQLClient,
+  variables: GetChatCommentsQueryVariables,
+  options?: UseQueryOptions<GetChatCommentsQuery, TError, TData>,
+  headers?: RequestInit["headers"]
+) =>
+  useQuery<GetChatCommentsQuery, TError, TData>(
+    ["GetChatComments", variables],
+    fetcher<GetChatCommentsQuery, GetChatCommentsQueryVariables>(
+      client,
+      GetChatCommentsDocument,
+      variables,
+      headers
+    ),
+    options
+  );
 
-useGetChatCommentsQuery.getKey = (variables: GetChatCommentsQueryVariables) => ['GetChatComments', variables];
-;
-
-useGetChatCommentsQuery.fetcher = (client: GraphQLClient, variables: GetChatCommentsQueryVariables, headers?: RequestInit['headers']) => fetcher<GetChatCommentsQuery, GetChatCommentsQueryVariables>(client, GetChatCommentsDocument, variables, headers);
+useGetChatCommentsQuery.getKey = (variables: GetChatCommentsQueryVariables) => [
+  "GetChatComments",
+  variables,
+];
+useGetChatCommentsQuery.fetcher = (
+  client: GraphQLClient,
+  variables: GetChatCommentsQueryVariables,
+  headers?: RequestInit["headers"]
+) =>
+  fetcher<GetChatCommentsQuery, GetChatCommentsQueryVariables>(
+    client,
+    GetChatCommentsDocument,
+    variables,
+    headers
+  );
 export const InsertChatCommentDocument = `
     mutation InsertChatComment($object: chat_comments_insert_input!) {
   insert_chat_comments_one(object: $object) {
@@ -95,19 +177,45 @@ export const InsertChatCommentDocument = `
 }
     `;
 export const useInsertChatCommentMutation = <
-      TError = unknown,
-      TContext = unknown
-    >(
-      client: GraphQLClient,
-      options?: UseMutationOptions<InsertChatCommentMutation, TError, InsertChatCommentMutationVariables, TContext>,
-      headers?: RequestInit['headers']
-    ) =>
-    useMutation<InsertChatCommentMutation, TError, InsertChatCommentMutationVariables, TContext>(
-      ['InsertChatComment'],
-      (variables?: InsertChatCommentMutationVariables) => fetcher<InsertChatCommentMutation, InsertChatCommentMutationVariables>(client, InsertChatCommentDocument, variables, headers)(),
-      options
-    );
-useInsertChatCommentMutation.fetcher = (client: GraphQLClient, variables: InsertChatCommentMutationVariables, headers?: RequestInit['headers']) => fetcher<InsertChatCommentMutation, InsertChatCommentMutationVariables>(client, InsertChatCommentDocument, variables, headers);
+  TError = unknown,
+  TContext = unknown
+>(
+  client: GraphQLClient,
+  options?: UseMutationOptions<
+    InsertChatCommentMutation,
+    TError,
+    InsertChatCommentMutationVariables,
+    TContext
+  >,
+  headers?: RequestInit["headers"]
+) =>
+  useMutation<
+    InsertChatCommentMutation,
+    TError,
+    InsertChatCommentMutationVariables,
+    TContext
+  >(
+    ["InsertChatComment"],
+    (variables?: InsertChatCommentMutationVariables) =>
+      fetcher<InsertChatCommentMutation, InsertChatCommentMutationVariables>(
+        client,
+        InsertChatCommentDocument,
+        variables,
+        headers
+      )(),
+    options
+  );
+useInsertChatCommentMutation.fetcher = (
+  client: GraphQLClient,
+  variables: InsertChatCommentMutationVariables,
+  headers?: RequestInit["headers"]
+) =>
+  fetcher<InsertChatCommentMutation, InsertChatCommentMutationVariables>(
+    client,
+    InsertChatCommentDocument,
+    variables,
+    headers
+  );
 export const SubscriptionChatCommentsDocument = `
     subscription SubscriptionChatComments($episode_id: uuid!) {
   chat_comments(where: {episode_id: {_eq: $episode_id}}) {
--- src/graphql/episode/episodeQuery.generated.ts
+++ src/graphql/episode/episodeQuery.generated.ts
@@ -1,39 +1,106 @@
-import * as Types from '../../types/graphql';
+import * as Types from "../../types/graphql";
 
-import { GraphQLClient } from 'graphql-request';
-import { RequestInit } from 'graphql-request/dist/types.dom';
-import { useMutation, useQuery, UseMutationOptions, UseQueryOptions } from '@tanstack/react-query';
+import { GraphQLClient } from "graphql-request";
+import { RequestInit } from "graphql-request/dist/types.dom";
+import {
+  useMutation,
+  useQuery,
+  UseMutationOptions,
+  UseQueryOptions,
+} from "@tanstack/react-query";
 
-function fetcher<TData, TVariables extends { [key: string]: any }>(client: GraphQLClient, query: string, variables?: TVariables, requestHeaders?: RequestInit['headers']) {
-  return async (): Promise<TData> => client.request({
-    document: query,
-    variables,
-    requestHeaders
-  });
+function fetcher<TData, TVariables extends { [key: string]: any }>(
+  client: GraphQLClient,
+  query: string,
+  variables?: TVariables,
+  requestHeaders?: RequestInit["headers"]
+) {
+  return async (): Promise<TData> =>
+    client.request({
+      document: query,
+      variables,
+      requestHeaders,
+    });
 }
 export type UpdateTodayEpisodeMutationVariables = Types.Exact<{
-  tid: Types.Scalars['Int'];
-  number: Types.Scalars['Int'];
+  tid: Types.Scalars["Int"];
+  number: Types.Scalars["Int"];
   episodes_set_input: Types.Episodes_Set_Input;
 }>;
 
-
-export type UpdateTodayEpisodeMutation = { __typename?: 'mutation_root', update_episodes?: { __typename?: 'episodes_mutation_response', returning: Array<{ __typename?: 'episodes', id: any, start_time?: any | null, end_time?: any | null }> } | null };
+export type UpdateTodayEpisodeMutation = {
+  __typename?: "mutation_root";
+  update_episodes?: {
+    __typename?: "episodes_mutation_response";
+    returning: Array<{
+      __typename?: "episodes";
+      id: any;
+      start_time?: any | null;
+      end_time?: any | null;
+    }>;
+  } | null;
+};
 
 export type GetTodayEpisodesQueryVariables = Types.Exact<{
   where: Types.Episodes_Bool_Exp;
 }>;
 
-
-export type GetTodayEpisodesQuery = { __typename?: 'query_root', episodes: Array<{ __typename?: 'episodes', id: any, title: string, end_time?: any | null, start_time?: any | null, number: number, has_next_episode: boolean, has_prev_episode: boolean, work: { __typename?: 'works', series_title: string, title: string, id: number, series_id?: string | null, tid?: number | null }, episode_likes_aggregate: { __typename?: 'episode_likes_aggregate', aggregate?: { __typename?: 'episode_likes_aggregate_fields', count: number } | null } }> };
+export type GetTodayEpisodesQuery = {
+  __typename?: "query_root";
+  episodes: Array<{
+    __typename?: "episodes";
+    id: any;
+    title: string;
+    end_time?: any | null;
+    start_time?: any | null;
+    number: number;
+    has_next_episode: boolean;
+    has_prev_episode: boolean;
+    work: {
+      __typename?: "works";
+      series_title: string;
+      title: string;
+      id: number;
+      series_id?: string | null;
+      tid?: number | null;
+    };
+    episode_likes_aggregate: {
+      __typename?: "episode_likes_aggregate";
+      aggregate?: {
+        __typename?: "episode_likes_aggregate_fields";
+        count: number;
+      } | null;
+    };
+  }>;
+};
 
 export type GetEpisodeQueryVariables = Types.Exact<{
-  id: Types.Scalars['uuid'];
+  id: Types.Scalars["uuid"];
 }>;
 
-
-export type GetEpisodeQuery = { __typename?: 'query_root', episodes_by_pk?: { __typename?: 'episodes', id: any, title: string, end_time?: any | null, start_time?: any | null, number: number, has_next_episode: boolean, has_prev_episode: boolean, next_episode_id?: any | null, prev_episode_id?: any | null, work: { __typename?: 'works', series_title: string, title: string, id: number, series_id?: string | null, tid?: number | null } } | null };
-
+export type GetEpisodeQuery = {
+  __typename?: "query_root";
+  episodes_by_pk?: {
+    __typename?: "episodes";
+    id: any;
+    title: string;
+    end_time?: any | null;
+    start_time?: any | null;
+    number: number;
+    has_next_episode: boolean;
+    has_prev_episode: boolean;
+    next_episode_id?: any | null;
+    prev_episode_id?: any | null;
+    work: {
+      __typename?: "works";
+      series_title: string;
+      title: string;
+      id: number;
+      series_id?: string | null;
+      tid?: number | null;
+    };
+  } | null;
+};
 
 export const UpdateTodayEpisodeDocument = `
     mutation UpdateTodayEpisode($tid: Int!, $number: Int!, $episodes_set_input: episodes_set_input!) {
@@ -50,19 +117,45 @@ export const UpdateTodayEpisodeDocument = `
 }
     `;
 export const useUpdateTodayEpisodeMutation = <
-      TError = unknown,
-      TContext = unknown
-    >(
-      client: GraphQLClient,
-      options?: UseMutationOptions<UpdateTodayEpisodeMutation, TError, UpdateTodayEpisodeMutationVariables, TContext>,
-      headers?: RequestInit['headers']
-    ) =>
-    useMutation<UpdateTodayEpisodeMutation, TError, UpdateTodayEpisodeMutationVariables, TContext>(
-      ['UpdateTodayEpisode'],
-      (variables?: UpdateTodayEpisodeMutationVariables) => fetcher<UpdateTodayEpisodeMutation, UpdateTodayEpisodeMutationVariables>(client, UpdateTodayEpisodeDocument, variables, headers)(),
-      options
-    );
-useUpdateTodayEpisodeMutation.fetcher = (client: GraphQLClient, variables: UpdateTodayEpisodeMutationVariables, headers?: RequestInit['headers']) => fetcher<UpdateTodayEpisodeMutation, UpdateTodayEpisodeMutationVariables>(client, UpdateTodayEpisodeDocument, variables, headers);
+  TError = unknown,
+  TContext = unknown
+>(
+  client: GraphQLClient,
+  options?: UseMutationOptions<
+    UpdateTodayEpisodeMutation,
+    TError,
+    UpdateTodayEpisodeMutationVariables,
+    TContext
+  >,
+  headers?: RequestInit["headers"]
+) =>
+  useMutation<
+    UpdateTodayEpisodeMutation,
+    TError,
+    UpdateTodayEpisodeMutationVariables,
+    TContext
+  >(
+    ["UpdateTodayEpisode"],
+    (variables?: UpdateTodayEpisodeMutationVariables) =>
+      fetcher<UpdateTodayEpisodeMutation, UpdateTodayEpisodeMutationVariables>(
+        client,
+        UpdateTodayEpisodeDocument,
+        variables,
+        headers
+      )(),
+    options
+  );
+useUpdateTodayEpisodeMutation.fetcher = (
+  client: GraphQLClient,
+  variables: UpdateTodayEpisodeMutationVariables,
+  headers?: RequestInit["headers"]
+) =>
+  fetcher<UpdateTodayEpisodeMutation, UpdateTodayEpisodeMutationVariables>(
+    client,
+    UpdateTodayEpisodeDocument,
+    variables,
+    headers
+  );
 export const GetTodayEpisodesDocument = `
     query GetTodayEpisodes($where: episodes_bool_exp!) {
   episodes(where: $where, order_by: {start_time: asc}) {
@@ -89,24 +182,39 @@ export const GetTodayEpisodesDocument = `
 }
     `;
 export const useGetTodayEpisodesQuery = <
-      TData = GetTodayEpisodesQuery,
-      TError = unknown
-    >(
-      client: GraphQLClient,
-      variables: GetTodayEpisodesQueryVariables,
-      options?: UseQueryOptions<GetTodayEpisodesQuery, TError, TData>,
-      headers?: RequestInit['headers']
-    ) =>
-    useQuery<GetTodayEpisodesQuery, TError, TData>(
-      ['GetTodayEpisodes', variables],
-      fetcher<GetTodayEpisodesQuery, GetTodayEpisodesQueryVariables>(client, GetTodayEpisodesDocument, variables, headers),
-      options
-    );
+  TData = GetTodayEpisodesQuery,
+  TError = unknown
+>(
+  client: GraphQLClient,
+  variables: GetTodayEpisodesQueryVariables,
+  options?: UseQueryOptions<GetTodayEpisodesQuery, TError, TData>,
+  headers?: RequestInit["headers"]
+) =>
+  useQuery<GetTodayEpisodesQuery, TError, TData>(
+    ["GetTodayEpisodes", variables],
+    fetcher<GetTodayEpisodesQuery, GetTodayEpisodesQueryVariables>(
+      client,
+      GetTodayEpisodesDocument,
+      variables,
+      headers
+    ),
+    options
+  );
 
-useGetTodayEpisodesQuery.getKey = (variables: GetTodayEpisodesQueryVariables) => ['GetTodayEpisodes', variables];
-;
-
-useGetTodayEpisodesQuery.fetcher = (client: GraphQLClient, variables: GetTodayEpisodesQueryVariables, headers?: RequestInit['headers']) => fetcher<GetTodayEpisodesQuery, GetTodayEpisodesQueryVariables>(client, GetTodayEpisodesDocument, variables, headers);
+useGetTodayEpisodesQuery.getKey = (
+  variables: GetTodayEpisodesQueryVariables
+) => ["GetTodayEpisodes", variables];
+useGetTodayEpisodesQuery.fetcher = (
+  client: GraphQLClient,
+  variables: GetTodayEpisodesQueryVariables,
+  headers?: RequestInit["headers"]
+) =>
+  fetcher<GetTodayEpisodesQuery, GetTodayEpisodesQueryVariables>(
+    client,
+    GetTodayEpisodesDocument,
+    variables,
+    headers
+  );
 export const GetEpisodeDocument = `
     query GetEpisode($id: uuid!) {
   episodes_by_pk(id: $id) {
@@ -129,22 +237,35 @@ export const GetEpisodeDocument = `
   }
 }
     `;
-export const useGetEpisodeQuery = <
-      TData = GetEpisodeQuery,
-      TError = unknown
-    >(
-      client: GraphQLClient,
-      variables: GetEpisodeQueryVariables,
-      options?: UseQueryOptions<GetEpisodeQuery, TError, TData>,
-      headers?: RequestInit['headers']
-    ) =>
-    useQuery<GetEpisodeQuery, TError, TData>(
-      ['GetEpisode', variables],
-      fetcher<GetEpisodeQuery, GetEpisodeQueryVariables>(client, GetEpisodeDocument, variables, headers),
-      options
-    );
+export const useGetEpisodeQuery = <TData = GetEpisodeQuery, TError = unknown>(
+  client: GraphQLClient,
+  variables: GetEpisodeQueryVariables,
+  options?: UseQueryOptions<GetEpisodeQuery, TError, TData>,
+  headers?: RequestInit["headers"]
+) =>
+  useQuery<GetEpisodeQuery, TError, TData>(
+    ["GetEpisode", variables],
+    fetcher<GetEpisodeQuery, GetEpisodeQueryVariables>(
+      client,
+      GetEpisodeDocument,
+      variables,
+      headers
+    ),
+    options
+  );
 
-useGetEpisodeQuery.getKey = (variables: GetEpisodeQueryVariables) => ['GetEpisode', variables];
-;
-
-useGetEpisodeQuery.fetcher = (client: GraphQLClient, variables: GetEpisodeQueryVariables, headers?: RequestInit['headers']) => fetcher<GetEpisodeQuery, GetEpisodeQueryVariables>(client, GetEpisodeDocument, variables, headers);
+useGetEpisodeQuery.getKey = (variables: GetEpisodeQueryVariables) => [
+  "GetEpisode",
+  variables,
+];
+useGetEpisodeQuery.fetcher = (
+  client: GraphQLClient,
+  variables: GetEpisodeQueryVariables,
+  headers?: RequestInit["headers"]
+) =>
+  fetcher<GetEpisodeQuery, GetEpisodeQueryVariables>(
+    client,
+    GetEpisodeDocument,
+    variables,
+    headers
+  );
