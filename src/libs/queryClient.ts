fb0ccf737bb214fc8b574bb48fd2d44537fb9d50
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-04 16:48:24 +0900

    fix quueryclient defult options

--- src/libs/queryClient.ts
+++ src/libs/queryClient.ts
@@ -1,11 +1,6 @@
-import { QueryCache, QueryClient } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: (error, query) => {
-      console.log(query);
-    },
-  }),
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
