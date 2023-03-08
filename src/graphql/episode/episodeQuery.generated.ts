459ded07d34e129e1e686973cfeb50db2ed21f05
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-08 12:47:48 +0900

    delete unused

--- src/graphql/episode/episodeQuery.generated.ts
+++ src/graphql/episode/episodeQuery.generated.ts
@@ -2,7 +2,7 @@ import * as Types from '../../types/graphql';
 
 import { GraphQLClient } from 'graphql-request';
 import { RequestInit } from 'graphql-request/dist/types.dom';
-import { useMutation, useQuery, UseMutationOptions, UseQueryOptions } from '@tanstack/react-query';
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 
 function fetcher<TData, TVariables extends { [key: string]: any }>(client: GraphQLClient, query: string, variables?: TVariables, requestHeaders?: RequestInit['headers']) {
   return async (): Promise<TData> => client.request({
@@ -11,15 +11,6 @@ function fetcher<TData, TVariables extends { [key: string]: any }>(client: Graph
     requestHeaders
   });
 }
-export type UpdateTodayEpisodeMutationVariables = Types.Exact<{
-  tid: Types.Scalars['Int'];
-  number: Types.Scalars['Int'];
-  episodes_set_input: Types.Episodes_Set_Input;
-}>;
-
-
-export type UpdateTodayEpisodeMutation = { __typename?: 'mutation_root', update_episodes?: { __typename?: 'episodes_mutation_response', returning: Array<{ __typename?: 'episodes', id: any, start_time?: any | null, end_time?: any | null }> } | null };
-
 export type GetTodayEpisodesQueryVariables = Types.Exact<{
   where: Types.Episodes_Bool_Exp;
 }>;
@@ -35,34 +26,6 @@ export type GetEpisodeQueryVariables = Types.Exact<{
 export type GetEpisodeQuery = { __typename?: 'query_root', episodes_by_pk?: { __typename?: 'episodes', id: any, title: string, end_time?: any | null, start_time?: any | null, number: number, has_next_episode: boolean, next_episode_id?: any | null, work: { __typename?: 'works', series_title: string } } | null };
 
 
-export const UpdateTodayEpisodeDocument = `
-    mutation UpdateTodayEpisode($tid: Int!, $number: Int!, $episodes_set_input: episodes_set_input!) {
-  update_episodes(
-    where: {_and: {number: {_eq: $number}, work: {tid: {_eq: $tid}}}}
-    _set: $episodes_set_input
-  ) {
-    returning {
-      id
-      start_time
-      end_time
-    }
-  }
-}
-    `;
-export const useUpdateTodayEpisodeMutation = <
-      TError = unknown,
-      TContext = unknown
-    >(
-      client: GraphQLClient,
-      options?: UseMutationOptions<UpdateTodayEpisodeMutation, TError, UpdateTodayEpisodeMutationVariables, TContext>,
-      headers?: RequestInit['headers']
-    ) =>
-    useMutation<UpdateTodayEpisodeMutation, TError, UpdateTodayEpisodeMutationVariables, TContext>(
-      ['UpdateTodayEpisode'],
-      (variables?: UpdateTodayEpisodeMutationVariables) => fetcher<UpdateTodayEpisodeMutation, UpdateTodayEpisodeMutationVariables>(client, UpdateTodayEpisodeDocument, variables, headers)(),
-      options
-    );
-useUpdateTodayEpisodeMutation.fetcher = (client: GraphQLClient, variables: UpdateTodayEpisodeMutationVariables, headers?: RequestInit['headers']) => fetcher<UpdateTodayEpisodeMutation, UpdateTodayEpisodeMutationVariables>(client, UpdateTodayEpisodeDocument, variables, headers);
 export const GetTodayEpisodesDocument = `
     query GetTodayEpisodes($where: episodes_bool_exp!) {
   episodes(where: $where, order_by: {start_time: asc}) {
@@ -97,10 +60,6 @@ export const useGetTodayEpisodesQuery = <
       fetcher<GetTodayEpisodesQuery, GetTodayEpisodesQueryVariables>(client, GetTodayEpisodesDocument, variables, headers),
       options
     );
-
-useGetTodayEpisodesQuery.getKey = (variables: GetTodayEpisodesQueryVariables) => ['GetTodayEpisodes', variables];
-;
-
 useGetTodayEpisodesQuery.fetcher = (client: GraphQLClient, variables: GetTodayEpisodesQueryVariables, headers?: RequestInit['headers']) => fetcher<GetTodayEpisodesQuery, GetTodayEpisodesQueryVariables>(client, GetTodayEpisodesDocument, variables, headers);
 export const GetEpisodeDocument = `
     query GetEpisode($id: uuid!) {
@@ -132,8 +91,4 @@ export const useGetEpisodeQuery = <
       fetcher<GetEpisodeQuery, GetEpisodeQueryVariables>(client, GetEpisodeDocument, variables, headers),
       options
     );
-
-useGetEpisodeQuery.getKey = (variables: GetEpisodeQueryVariables) => ['GetEpisode', variables];
-;
-
 useGetEpisodeQuery.fetcher = (client: GraphQLClient, variables: GetEpisodeQueryVariables, headers?: RequestInit['headers']) => fetcher<GetEpisodeQuery, GetEpisodeQueryVariables>(client, GetEpisodeDocument, variables, headers);
