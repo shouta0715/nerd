ab7468eb425aac96b20728082b7ede94536698a1
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-10 01:22:23 +0900

    refix delete dynamic

--- src/pages/episode/[slug].tsx
+++ src/pages/episode/[slug].tsx
@@ -1,24 +1,13 @@
 import { Box } from "@mantine/core";
 import { NextPage } from "next";
-import dynamic from "next/dynamic";
 import React, { Suspense } from "react";
 import { EpisodeSkelton } from "src/components/Layout/loading/EpisodeSkelton";
-
-const DynamicEpisode = dynamic(
-  () =>
-    import("src/features/episodes/components/Episode").then(
-      (mod) => mod.Episode
-    ),
-  {
-    ssr: true,
-    suspense: true,
-  }
-);
+import { Episode } from "src/features/episodes/components/Episode";
 
 const Index: NextPage = () => (
   <Box component="section">
     <Suspense fallback={<EpisodeSkelton />}>
-      <DynamicEpisode />
+      <Episode />
     </Suspense>
   </Box>
 );
