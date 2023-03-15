59bb243ce1faf57802c898b605d631853895605e
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-15 21:49:14 +0900

    fix strokeWidth

--- src/components/Icon/RotateArrowRightIcon.tsx
+++ src/components/Icon/RotateArrowRightIcon.tsx
@@ -6,7 +6,7 @@ export const RotateArrowRightIcon = ({ className = "" }) => (
     data-name="Layer 1"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    strokeWidth="1.5"
     className={className}
   >
     <defs>
