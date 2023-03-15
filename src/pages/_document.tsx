37f26c4c87795dcea8ad3f318ef72861e22664d7
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-15 18:16:14 +0900

    delete mantine

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,33 +1,23 @@
-import { createGetInitialProps } from "@mantine/next";
-import Documents, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-const getInitialProps = createGetInitialProps();
+const Document = () => (
+  <Html>
+    <Head>
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="常にリアルタイムで感想を共有しよう" />
+      <meta property="og:site_name" content="Anime" />
+      <meta property="og:title" content="Anime" />
+      <meta
+        property="og:description"
+        content="常にリアルタイムで感想を共有しよう"
+      />
+    </Head>
+    <body className="font-futura">
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
 
-export default class _Document extends Documents {
-  static getInitialProps = getInitialProps;
-
-  render() {
-    return (
-      <Html lang="ja">
-        <Head>
-          <meta charSet="utf-8" />
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="常にリアルタイムで感想を共有しよう"
-          />
-          <meta property="og:site_name" content="Anime" />
-          <meta property="og:title" content="Anime" />
-          <meta
-            property="og:description"
-            content="常にリアルタイムで感想を共有しよう"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+export default Document;
