8cedf4c5066c98b679eac605f97ac29ad5310266
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-22 23:28:37 +0900

    fix config

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: "@svgr/webpack",
-        },
-      ],
-    });
-    return config;
-  },
-  images: {
-    disableStaticImages: true, // importした画像の型定義設定を無効にする
-  },
   experimental: {
     scrollRestoration: true,
   },
