9c78438984ea748dbcfe2c42a140cfba40065b3b
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-27 20:10:39 +0900

    add series skelton

--- src/components/Layout/loading/WorkSkelton.tsx
+++ src/components/Layout/loading/WorkSkelton.tsx
@@ -2,10 +2,15 @@
 import React from "react";
 import { WorkEpisodeSkelton } from "src/components/Layout/loading/WorkEpisodeSkelton";
 
-export const WorkSkelton = ({ is_short = false }) => (
+export const WorkSkelton = ({ is_short = false, isButton = false }) => (
   <ul className="mx-auto grid w-full grid-cols-2 space-y-1 rounded-md border border-solid border-slate-200 bg-white p-3 shadow md:grid-cols-3 md:p-4 lg:grid-cols-4">
     {Array.from({ length: is_short ? 8 : 24 }).map((_, index) => (
       <WorkEpisodeSkelton key={`work-episode-skelton-${index}`} />
     ))}
+    {isButton && (
+      <div className="col-span-2 mx-auto mt-4 flex w-max animate-pulse justify-center md:col-span-3 lg:col-span-4">
+        <div className=" h-3 w-[200px] rounded-md bg-slate-200" />
+      </div>
+    )}
   </ul>
 );
--- src/pages/series/[slug].tsx
+++ src/pages/series/[slug].tsx
@@ -1,10 +1,11 @@
 import { NextPage } from "next";
 import React, { Suspense } from "react";
+import { SeriesSkelton } from "src/components/Layout/loading/SeriesSkelton";
 import { Series } from "src/features/series/components/Series";
 
 const Index: NextPage = () => (
   <section className="flex min-h-screen flex-col">
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<SeriesSkelton />}>
       <Series />
     </Suspense>
   </section>
