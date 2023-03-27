4ddfc5adde53b20c3aca3fb9f96af0f14a8ba231
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-27 15:07:32 +0900

    fix nav style

--- src/components/Layout/Navigation.tsx
+++ src/components/Layout/Navigation.tsx
@@ -16,51 +16,42 @@ const mockNavigation = [
     name: "今日放送",
     href: "/list/todayEpisodes",
     icon: TvIcon,
-    color: " stroke-indigo-500",
   },
   {
     name: "今期のアニメ",
     href: "/list/seasonWorks",
     icon: RocketLaunchIcon,
-    color: " stroke-red-500",
   },
 
   {
     name: "作品要望",
     href: "/",
     icon: DocumentArrowUpIcon,
-    color: "stroke-green-500",
   },
   {
     name: "マイリスト",
     href: "/",
     icon: ListBulletIcon,
-    color: "fill-orange-500 stroke-orange-500",
   },
   {
     name: "設定",
     href: "/",
     icon: Cog8ToothIcon,
-    color: "stroke-gray-500",
   },
   {
     name: "使い方",
     href: "/",
     icon: BeginnerIcon,
-    color: "stroke-blue-500",
   },
 ];
 
 const navList = mockNavigation.map((item) => (
   <li
     key={item.name}
-    className="group flex items-center  justify-center space-x-2 bg-white text-xs font-bold  md:text-sm"
+    className="group flex items-center  justify-center space-x-2 rounded-md  p-2 text-xs font-bold md:text-sm md:hover:bg-gray-100"
   >
-    <item.icon className={`h-full w-6 ${item.color}`} />
-    <Link
-      className="flex flex-1 items-center border-b py-2 group-hover:border-indigo-500"
-      href={`${item.href}`}
-    >
+    <item.icon className="h-full w-6" />
+    <Link className="flex flex-1 items-center" href={`${item.href}`}>
       <span className="mt-1 inline-block flex-1">{item.name}</span>
       <ChevronRightIcon className="h-4 w-4 stroke-slate-300 group-hover:stroke-indigo-500 md:h-6 md:w-6 " />
     </Link>
@@ -69,6 +60,6 @@ const navList = mockNavigation.map((item) => (
 
 export const Navigation: FC = () => (
   <nav>
-    <ul className="grid grid-cols-2 gap-4 md:grid-cols-1">{navList}</ul>
+    <ul className="grid grid-cols-2 gap-2 md:grid-cols-1">{navList}</ul>
   </nav>
 );
