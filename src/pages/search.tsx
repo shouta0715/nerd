5c55907cfd2ecb363ac6db8842c30dc215cfcb84
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-28 22:18:40 +0900

    add skelton

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,11 +1,12 @@
 import { NextPage } from "next";
 import React, { Suspense } from "react";
+import { SeriesSkelton } from "src/components/Elements/Loader/loaders/SeriesSkelton";
 import { SearchButton } from "src/components/Elements/SearchButton";
 import { SpSearchWorks } from "src/features/works/components/SpSearchWorks";
 
 const Search: NextPage = () => (
   <>
-    <Suspense>
+    <Suspense fallback={<SeriesSkelton />}>
       <SpSearchWorks />
     </Suspense>
     <SearchButton />
