73741e3743de40492a574249a4f4292db8804acf
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-31 13:23:25 +0900

    fix useInitilize

--- src/hooks/auth/useGoogleSignIn.ts
+++ src/hooks/auth/useGoogleSignIn.ts
@@ -1,15 +1,12 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { useRouter } from "next/router";
 import { auth } from "../../libs/firebase";
 
 export const useGoogleSignIn = () => {
-  const router = useRouter();
   const signInGoogle = async () => {
     const provider = new GoogleAuthProvider();
     try {
       await signInWithPopup(auth, provider).then((result) => result.user);
 
-      router.push("/");
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       if (!(error.message === "Firebase: Error (auth/popup-closed-by-user)."))
