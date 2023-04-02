8a5049e5d393f4f203213480cd68c8104bc681cc
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-04-02 17:37:21 +0900

    delete useTimer

--- src/features/timer/hooks/useLiveTimer.ts
+++ src/features/timer/hooks/useLiveTimer.ts
@@ -1,16 +1,67 @@
 /* eslint-disable consistent-return */
 import { useEffect, useRef, useState } from "react";
-import { useTimer } from "src/features/timer/hooks/useTimer";
-import { LiveTimer, LiveTimerProps, Time } from "src/features/timer/types";
+import {
+  CountDownProps,
+  CountUpProps,
+  LiveTimer,
+  LiveTimerProps,
+  Time,
+} from "src/features/timer/types";
 import { getInitialTime } from "src/features/timer/utils/getInitialTime";
 import { getIsStatus } from "src/features/timer/utils/getIsStatus";
 import { getMaxCountUpTime } from "src/features/timer/utils/getMaxCountUpTime";
+
+const countDown = ({ prevTime, setMode, intervalId }: CountDownProps): Time => {
+  const { hours, minutes, seconds } = prevTime;
+
+  if (hours === 0 && minutes === 0 && seconds === 0) {
+    setMode("up");
+
+    if (intervalId.current) clearInterval(intervalId.current);
+
+    return prevTime;
+  }
+
+  // 一秒ずつ減らしていく
+  return {
+    seconds: seconds === 0 ? 59 : seconds - 1,
+    minutes: seconds === 0 ? minutes - 1 : minutes,
+    hours: minutes === 0 ? hours - 1 : hours,
+  };
+};
+
+const countUp = ({
+  prevTime,
+  maxTime,
+  setMode,
+  intervalId,
+}: CountUpProps): Time => {
+  const { hours, minutes, seconds } = prevTime;
+
+  if (
+    hours === maxTime.hours &&
+    minutes === maxTime.minutes &&
+    seconds === maxTime.seconds
+  ) {
+    setMode("finish");
+
+    if (intervalId.current) clearInterval(intervalId.current);
+
+    return prevTime;
+  }
+
+  // 一秒ずつ増やしていく
+  return {
+    seconds: seconds === 59 ? 0 : seconds + 1,
+    minutes: seconds === 59 ? minutes + 1 : minutes,
+    hours: minutes === 59 ? hours + 1 : hours,
+  };
+};
 
 export const useLiveTimer = ({
   start_time,
   end_time,
 }: LiveTimerProps): LiveTimer => {
-  const { countDown, countUp } = useTimer();
   const [mode, setMode] = useState<LiveTimer["mode"]>(
     getIsStatus({ start_time, end_time })
   );
@@ -51,16 +102,7 @@ export const useLiveTimer = ({
     return () => {
       if (intervalId.current) clearInterval(intervalId.current);
     };
-  }, [
-    countDown,
-    countUp,
-    end_time,
-    mode,
-    start_time,
-    time.hours,
-    time.minutes,
-    time.seconds,
-  ]);
+  }, [end_time, mode, start_time, time.hours, time.minutes, time.seconds]);
 
   return {
     mode,
