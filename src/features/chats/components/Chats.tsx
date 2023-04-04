44f0c0fa8ce3926ca5e7eef5187d2bd406a56048
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-04-04 21:35:06 +0900

    add start button

--- src/features/chats/components/Chats.tsx
+++ src/features/chats/components/Chats.tsx
@@ -1,10 +1,13 @@
+import { Transition } from "@headlessui/react";
 import { ArrowDownIcon } from "@heroicons/react/24/outline";
+import { PlayIcon } from "@heroicons/react/24/solid";
 import React, { FC } from "react";
 import { Chat } from "src/features/chats/components/Chat";
-import { Chat as TypeeChat } from "src/features/chats/types";
+import { Chat as TypeChat } from "src/features/chats/types";
+import { useTimerState } from "src/features/timer/store/timerStore";
 
 type Props = {
-  chats: TypeeChat[];
+  chats: TypeChat[];
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   bottomRef: (element: any) => void;
   isBottom?: boolean;
@@ -18,23 +21,42 @@ export const Chats: FC<Props> = ({
   isBottom,
   entry,
   time,
-}) => (
-  <ul className="relative mx-auto w-full flex-1 space-y-3 px-4 pb-1 pt-4 md:max-w-xl">
-    {chats?.map((comment) => (
-      <Chat key={comment.id} animate chat={comment} />
-    ))}
-    <button
-      className={`fixed bottom-[4.5rem] left-1/2 z-0 flex h-7 w-7 -translate-x-1/2   cursor-pointer items-center  justify-center rounded-full border-none bg-indigo-500 shadow-md shadow-black/[0.3]  transition-all active:translate-y-1 ${
-        isBottom || time === 0
-          ? "translate-y-10 opacity-0"
-          : "opacity-1 translate-y-0"
-      }`}
-      onClick={() => {
-        entry?.target.scrollIntoView({ behavior: "smooth" });
-      }}
-    >
-      <ArrowDownIcon className="h-4 w-4 fill-white stroke-white stroke-2 text-white" />
-    </button>
-    <div ref={bottomRef} className="absolute bottom-0 opacity-0" />
-  </ul>
-);
+}) => {
+  const interval = useTimerState((state) => state.interval);
+
+  return (
+    <ul className="relative mx-auto flex w-full flex-1 flex-col space-y-3 px-4 pb-1 pt-4 md:max-w-xl">
+      <Transition
+        as="button"
+        className="m-auto grid place-items-center rounded-full border bg-black p-4 transition-all active:translate-y-0.5"
+        enter="duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-300"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        onClick={interval.start}
+        show={time === 0 && !interval.active}
+      >
+        <PlayIcon className="h-10 w-10 fill-white" />
+      </Transition>
+
+      {chats?.map((comment) => (
+        <Chat key={comment.id} animate chat={comment} />
+      ))}
+      <button
+        className={`fixed bottom-[4.5rem] left-1/2 z-0 flex h-7 w-7 -translate-x-1/2   cursor-pointer items-center  justify-center rounded-full border-none bg-indigo-500 shadow-md shadow-black/[0.3]  transition-all active:translate-y-1 ${
+          isBottom || time === 0
+            ? "translate-y-10 opacity-0"
+            : "opacity-1 translate-y-0"
+        }`}
+        onClick={() => {
+          entry?.target.scrollIntoView({ behavior: "smooth" });
+        }}
+      >
+        <ArrowDownIcon className="h-4 w-4 fill-white stroke-white stroke-2 text-white" />
+      </button>
+      <div ref={bottomRef} className="absolute bottom-0 opacity-0" />
+    </ul>
+  );
+};
