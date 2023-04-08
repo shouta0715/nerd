2b74e6c3616226943f68254b9c30eb7d5c1871e9
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-04-08 14:44:37 +0900

    add usage

--- src/pages/usage.tsx
+++ src/pages/usage.tsx
@@ -1,3 +1,10 @@
+import { HandThumbUpIcon } from "@heroicons/react/24/outline";
+import {
+  ChatBubbleLeftRightIcon,
+  MagnifyingGlassIcon,
+  PaperAirplaneIcon,
+  PlayIcon,
+} from "@heroicons/react/24/solid";
 import { NextPage } from "next";
 import React from "react";
 import { SearchModal } from "src/components/Elements/SearchModal";
@@ -11,30 +18,19 @@ const Usage: NextPage = () => {
   return (
     <>
       <Layout>
-        <div className="flex-1  bg-gray-50 py-4">
-          <h1 className=" px-3 font-hiragino-sans text-xl font-bold md:px-6">
-            <div className="flex items-center">
-              <div className="mr-2 rounded-xl bg-indigo-500 px-2.5 py-1 font-bold text-white">
-                Q
-              </div>
-              Nerd とは？
-            </div>
-            <p className="mt-2 font-hiragino-sans text-base font-normal">
-              Nerdとは、匿名またはログインしてアニメの感想を投稿できるサイトです。
-            </p>
-          </h1>
-          <section className="mt-4 border-t border-t-slate-200 px-3 font-hiragino-sans md:px-6">
-            <h2 className="py-4 text-center font-hiragino-sans text-xl font-bold">
+        <div className="flex-1 bg-gray-50 py-4">
+          <section className="px-3 font-hiragino-sans md:px-6">
+            <h2 className="pb-4 pt-2 text-center font-hiragino-sans text-xl font-bold">
               使い方
             </h2>
-            <ol className="">
-              <li className="relative flex pb-10">
-                <div className="absolute left-5 z-0 h-full w-px bg-slate-200" />
-                <div className="z-10 mr-4 grid h-10 w-10 place-items-center rounded-full border border-indigo-200 bg-white font-futura text-indigo-500">
-                  1
+            <ol className="text-sm md:text-base">
+              <li className="relative flex pb-8">
+                <div className="absolute left-5 z-0 h-full w-0.5 bg-slate-200" />
+                <div className="z-10 mr-4 grid h-10 w-10 place-items-center rounded-full bg-indigo-500 font-futura ">
+                  <MagnifyingGlassIcon className="h-6 w-6 fill-white stroke-white" />
                 </div>
-                <p className="flex-1 self-center">
-                  見るアニメを検索します。
+                <p className="flex-1 pt-1">
+                  見るアニメを検索し、エピソードまたはアニメを選択します。
                   <button
                     className="text-sm text-indigo-500 underline md:hidden"
                     onClick={() => setIsSearchOpen(!isSearchOpen)}
@@ -42,52 +38,76 @@ const Usage: NextPage = () => {
                     検索はこちら
                   </button>
                 </p>
+                <p className="ml-2 h-10 pt-1.5 text-xs text-gray-500  md:pt-2">
+                  Step 1. 検索
+                </p>
               </li>
-              <li className="relative flex pb-10">
-                <div className="absolute left-5 z-0 h-full w-px bg-slate-200" />
-                <div className="z-10 mr-4 grid h-10 w-10 place-items-center rounded-full border border-indigo-200 bg-white font-futura text-indigo-500">
-                  2
+              <li className="relative flex pb-8">
+                <div className="absolute left-5 z-0 h-full w-0.5 bg-slate-200" />
+                <div className="z-10 mr-4 grid h-10 w-10 place-items-center rounded-full bg-teal-500 font-futura ">
+                  <PlayIcon className="h-6 w-6 fill-white stroke-white" />
                 </div>
-                <p className="flex-1 self-center">
+                <p className="flex-1 pt-1">
                   チャットを選択し、動画配信サービスの再生ボタンと同時に再生ボタンをクリックします。
                 </p>
-              </li>
-              <li className="relative flex pb-10">
-                <div className="absolute left-5 z-0 h-full w-px bg-slate-200" />
-                <div className="z-10 mr-4 grid h-10 w-10 place-items-center rounded-full border border-indigo-200 bg-white font-futura text-indigo-500">
-                  3
-                </div>
-                <p className="flex-1 self-center">
-                  画面下部のコメント欄に感想を入力し、感想を送信します。
+                <p className="ml-2 h-10 pt-1.5 text-xs text-gray-500  md:pt-2">
+                  Step 2. 再生
                 </p>
               </li>
-              <li className="relative flex pb-10">
-                <div className="absolute left-5 z-0 h-full w-px bg-slate-200" />
-                <div className="z-10 mr-4 grid h-10 w-10 place-items-center rounded-full border border-indigo-200 bg-white font-futura text-indigo-500">
-                  4
+              <li className="relative flex pb-8">
+                <div className="absolute left-5 z-0 h-full w-0.5 bg-slate-200" />
+                <div className="z-10 mr-4 grid h-10 w-10 place-items-center rounded-full bg-indigo-500 font-futura ">
+                  <PaperAirplaneIcon className="h-6 w-6 fill-white stroke-white" />
                 </div>
-                <p className="flex-1 self-center">
+                <p className="flex-1 pt-1">
+                  画面下部のコメント欄に感想を入力し、感想を送信します。
+                </p>
+
+                <p className="ml-2 h-10 pt-1.5 text-xs text-gray-500  md:pt-2">
+                  Step 3. 投稿
+                </p>
+              </li>
+              <li className="relative flex pb-8">
+                <div className="absolute left-5 z-0 h-full w-0.5 bg-slate-200" />
+                <div className="z-10 mr-4 grid h-10 w-10 place-items-center rounded-full bg-teal-500 font-futura ">
+                  <ChatBubbleLeftRightIcon className="h-6 w-6 fill-white stroke-white" />
+                </div>
+                <p className="flex-1 pt-1">
                   動画が終了したら、コメントタブを選択し、アニメ全体の感想を投稿できます。
                   <br />
                   <span className="text-sm underline decoration-pink-500 decoration-dashed underline-offset-2 before:inline-block before:content-['※']">
                     テレビ放送が終了しているアニメのコメントは、いつでも投稿できます。
                   </span>
                 </p>
+
+                <p className="ml-2 h-10 pt-1.5 text-xs text-gray-500  md:pt-2">
+                  Step 4. 投稿
+                </p>
               </li>
-              <li className="relative flex pb-10">
-                <div className="z-10 mr-4 grid h-10 w-10 place-items-center rounded-full border border-indigo-200 bg-white font-futura text-indigo-500">
-                  5
+              <li className="relative flex ">
+                <div className="z-10 mr-4 grid h-10 w-10 place-items-center rounded-full bg-red-500 font-futura ">
+                  <HandThumbUpIcon className="h-6 w-6 fill-white stroke-white" />
                 </div>
-                <p className="flex-1 self-center">
+                <p className="flex-1 pt-1">
                   投稿名を自由に変更して、いろいろなアニメの感想を投稿してみましょう。
+                </p>
+
+                <p className="ml-2 h-10 pt-1.5 text-xs text-gray-500  md:pt-2">
+                  Step 5. 変更
                 </p>
               </li>
             </ol>
           </section>
-          <section className="mt-4 border-t border-t-slate-200 px-3 font-hiragino-sans md:px-6">
+          <section className="mt-4 px-3 font-hiragino-sans md:px-6">
             <h2 className="py-4 text-center font-hiragino-sans text-xl font-bold">
-              ルール
+              ルール・注意事項
             </h2>
+            <ol className="list-inside list-decimal space-y-4">
+              <li>不適切なコメントは禁止です。</li>
+              <li>暴言や、誹謗中傷、個人情報、などの投稿は禁止です。</li>
+              <li>関係のないアニメの感想を投稿は禁止です。</li>
+              <li>悪意のあるネタバレは禁止です。</li>
+            </ol>
           </section>
         </div>
       </Layout>
