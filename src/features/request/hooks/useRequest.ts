c7e101d8f4c6cb81d0dbcc98c54faeda9245ffd4
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-04-23 22:11:43 +0900

    add reauest components

--- src/features/request/hooks/useRequest.ts
+++ src/features/request/hooks/useRequest.ts
@@ -2,7 +2,8 @@ import { useMutateRequest } from "src/features/request/api/useMutateRequest";
 
 export const useRequest = () => {
   const insertRequest = useMutateRequest();
-  const onSubmitHandler = async () => {
+  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     console.log("onSubmitHandler", insertRequest);
   };
 
