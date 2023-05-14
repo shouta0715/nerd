ad529fb81f86873c531003ca6bbf42204ef4b06d
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-05-14 17:30:26 +0900

    add ssg types

--- src/libs/next/types.ts
+++ src/libs/next/types.ts
@@ -1,7 +1,14 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import { AppProps } from "next/app";
 import { ReactElement, ReactNode } from "react";
+import { AutoCompleteData } from "src/features/episodes/types";
+import { GetTodayEpisodesQuery } from "src/graphql/episode/episodeQuery.generated";
+import {
+  GetSeasonWorksQuery,
+  GetWeeklyWorksQuery,
+} from "src/graphql/work/workQuery.generated";
+import { Error } from "src/libs/error";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout: (page: ReactElement) => ReactNode;
@@ -11,3 +18,30 @@ export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 export type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
+
+export type NextSSGResult<T> = {
+  data: T;
+  error: Error | null;
+};
+
+export type TopPageList<T> = {
+  data: T;
+  buildDate: string;
+};
+
+export type TopPage = {
+  todayData: GetTodayEpisodesQuery;
+  seasonData: GetSeasonWorksQuery;
+  weeklyData: GetWeeklyWorksQuery;
+  buildDate: string;
+  registeredError: Error | null;
+};
+
+export type ListPage<T> = {
+  data: T;
+  autoCompleteData: AutoCompleteData[];
+};
+
+export type NextSSG<T> = GetStaticProps<NextSSGResult<T>>;
+
+export type NextSSGPage<T> = NextPageWithLayout<NextSSGResult<T>>;
