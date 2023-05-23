28e3ff528bab578ef57289eccc63310ea99bc427
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-05-23 13:11:50 +0900

    udapte bg

--- src/components/Elements/Background/index.tsx
+++ src/components/Elements/Background/index.tsx
@@ -5,7 +5,7 @@ export const Background = () => {
     <>
       <div
         aria-hidden="true"
-        className="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl"
+        className="fixed  inset-x-0 top-1/2 -z-10 max-h-full -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl"
       >
         <div
           className="ml-[max(50%,38rem)] aspect-[1313/771] w-[82.0625rem] bg-gradient-to-tr from-[#9780ff] via-[#89fcdd] to-[#fcfc89]"
@@ -17,10 +17,10 @@ export const Background = () => {
       </div>
       <div
         aria-hidden="true"
-        className="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end"
+        className="fixed inset-x-0 top-0 -z-10 flex max-h-full transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end"
       >
         <div
-          className="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-[#9780ff] via-[#89fcdd] to-[#fcfc89] xl:ml-0 xl:mr-[calc(50%-12rem)]"
+          className="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-[#ffff82] via-[#9dffcc] to-[#896ffb] xl:ml-0 xl:mr-[calc(50%-12rem)]"
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
--- src/components/Layouts/Aside/index.tsx
+++ src/components/Layouts/Aside/index.tsx
@@ -4,7 +4,7 @@ import { SearchWorks } from "src/features/works/components/SearchWorks";
 import { SearchWorksForm } from "src/features/works/components/SearchWorksForm";
 
 export const Aside = () => (
-  <aside className="static top-0 w-full shrink-0 bg-white/25  pt-8 md:sticky md:block md:w-56">
+  <aside className="static top-0 w-full shrink-0 border-b border-b-slate-200 bg-white/30 md:sticky  md:block md:w-56 md:border-b-0 md:pt-8">
     <div className="container mx-auto px-2 md:space-y-4 md:px-4">
       <SearchWorksForm />
       <Nav />
