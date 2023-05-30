8d28fa8d10b4beeb3765a0f77c0393929745b33b
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-05-30 13:53:37 +0900

    fix auth error

--- src/components/Modal/Delete/index.tsx
+++ src/components/Modal/Delete/index.tsx
@@ -46,7 +46,6 @@ export const DeleteModal: FC<Props> = ({ open, onClose }) => {
         </Button>
         <button
           className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-          disabled={authLoading}
           onClick={onClose}
           type="button"
         >
