8551595996704880d7f9f0d51af7042a612b5770
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-06-13 21:05:44 +0900

    auth error

---
+++ src/features/auth/hooks/useSignIn.ts
@@ -0,0 +1,47 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useCallback } from "react";
+import { useNotificationState } from "src/components/Elements/Notification/store";
+import { UnauthorizedError } from "src/libs/error";
+import { auth } from "src/libs/firebase";
+import { useGlobalState } from "src/store/global/globalStore";
+import { useUserState } from "src/store/user/userState";
+
+export const useSignIn = () => {
+  const [authLoading, setAuthLoading] = useGlobalState((state) => [
+    state.authLoading,
+    state.setAuthLoading,
+  ]);
+
+  const onShow = useNotificationState((state) => state.onShow);
+  const user = useUserState((state) => state.user);
+
+  const signInGoogle = useCallback(async () => {
+    console.log("signInGoogle");
+    setAuthLoading(true);
+    const provider = new GoogleAuthProvider();
+
+    try {
+      if (!auth.currentUser) throw new UnauthorizedError();
+
+      await signInWithPopup(auth, provider);
+      onShow({
+        title: "ログインしました",
+        type: "success",
+      });
+    } catch (error: unknown) {
+      setAuthLoading(false);
+      if (error instanceof UnauthorizedError) {
+        onShow({
+          title: "ログインに失敗しました",
+          type: "error",
+        });
+      }
+    } finally {
+      if (!user?.anonymous && authLoading) {
+        setAuthLoading(false);
+      }
+    }
+  }, [authLoading, onShow, setAuthLoading, user?.anonymous]);
+
+  return { signInGoogle };
+};
---
+++ src/features/auth/hooks/useSignOut.ts
@@ -0,0 +1,33 @@
+import { signOut } from "firebase/auth";
+import { useCallback } from "react";
+import { useNotificationState } from "src/components/Elements/Notification/store";
+import { auth } from "src/libs/firebase";
+import { useGlobalState } from "src/store/global/globalStore";
+
+export const useSignOut = () => {
+  const setAuthLoading = useGlobalState((state) => state.setAuthLoading);
+
+  const onShow = useNotificationState((state) => state.onShow);
+
+  const signOutGoogle = useCallback(async () => {
+    console.log("signOutGoogle");
+    setAuthLoading(true);
+
+    try {
+      await signOut(auth);
+      onShow({
+        title: "ログアウトしました",
+        type: "success",
+      });
+    } catch (error) {
+      onShow({
+        title: "ログアウトに失敗しました",
+        type: "error",
+      });
+    }
+
+    setAuthLoading(false);
+  }, [onShow, setAuthLoading]);
+
+  return { signOutGoogle };
+};
