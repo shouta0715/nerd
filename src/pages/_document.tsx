33b430725eff052f305595dc5542fdba1c1cf925
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-06-23 16:02:25 +0900

    update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,12 +14,12 @@ const Document = () => (
         content="匿名でアニメの感想を投稿してみんなと感想を共有しよう！"
         property="og:description"
       />
-      <link href="/favicon.ico" rel="icon" type="image/x-icon" />
       <link href="/logo.png" rel="apple-touch-icon" sizes="180x180" />
+      <link href="/logo.png" rel="icon" sizes="32x32" type="image/png" />
+      <link href="/logo.png" rel="icon" sizes="16x16" type="image/png" />
       <link href="/logo.svg" rel="icon" sizes="32x32" type="image/svg+xml" />
       <link href="/logo.svg" rel="icon" sizes="16x16" type="image/svg+xml" />
-      <link href="/site.webmanifest" rel="manifest" />
-      <link color="#5bbad5" href="/logo.svg" rel="mask-icon" />
+      <link color="#5bbad5" href="/logo.png" rel="mask-icon" />
       <meta content="#da532c" name="msapplication-TileColor" />
       <meta content="#ffffff" name="theme-color" />
     </Head>
