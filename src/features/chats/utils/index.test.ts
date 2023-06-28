a2584e1f9f3f2f63bd435f8fbc3cc2b2ceb852ef
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-06-28 10:08:02 +0900

    fix isBetween

--- src/features/chats/utils/index.test.ts
+++ src/features/chats/utils/index.test.ts
@@ -1,4 +1,4 @@
-import { multipleOf300 } from "src/features/chats/utils";
+import { isAvoidFetchNext, multipleOf300 } from "src/features/chats/utils";
 
 describe("chat/utils", () => {
   describe("multipleOf300", () => {
@@ -21,6 +21,67 @@ describe("chat/utils", () => {
       expect(multipleOf300(121)).toBe(300);
       expect(multipleOf300(299)).toBe(600);
       expect(multipleOf300(301)).toBe(600);
+    });
+  });
+
+  describe("isAvoidFetchNext", () => {
+    test("pageParamsの_gteがtimeより大きい場合はtrueを返す", () => {
+      expect(
+        isAvoidFetchNext(0, {
+          _gte: 1,
+          _lt: 300,
+        })
+      ).toBe(true);
+
+      expect(
+        isAvoidFetchNext(600, {
+          _gte: 900,
+          _lt: 1200,
+        })
+      ).toBe(true);
+    });
+
+    test("pageParamsの間にtimeがいる場合はtrue", () => {
+      expect(
+        isAvoidFetchNext(600, {
+          _gte: 300,
+          _lt: 900,
+        })
+      ).toBe(true);
+    });
+
+    test("pageParamsの間にtimeがいない場合はfalse", () => {
+      expect(
+        isAvoidFetchNext(600, {
+          _gte: 300,
+          _lt: 600,
+        })
+      ).toBe(false);
+    });
+
+    test("pageParamsの_ltがtimeより小さい場合はfalseを返す", () => {
+      expect(
+        isAvoidFetchNext(900, {
+          _gte: 300,
+          _lt: 600,
+        })
+      ).toBe(false);
+    });
+
+    test("pageParamsの_ltがtimeより大きい場合はtrue", () => {
+      expect(
+        isAvoidFetchNext(300, {
+          _gte: 600,
+          _lt: 900,
+        })
+      ).toBe(true);
+
+      expect(
+        isAvoidFetchNext(899, {
+          _gte: 600,
+          _lt: 900,
+        })
+      ).toBe(true);
     });
   });
 });
--- src/features/chats/utils/index.ts
+++ src/features/chats/utils/index.ts
@@ -2,8 +2,10 @@ import { PageParams } from "src/features/chats/types";
 import { MaxTime } from "src/features/timer/store/initialState";
 import { timeToSecond } from "src/features/timer/utils/timeProcessing";
 
-export const isBetweenTime = (time: number, pageParams: PageParams) => {
+export const isAvoidFetchNext = (time: number, pageParams: PageParams) => {
   const { _gte, _lt } = pageParams;
+
+  if (_gte > time) return true;
 
   return time >= _gte && time < _lt;
 };
