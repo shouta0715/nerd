0dc0c7f4a7265be701033aa65f5327cc0d60d0f0
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-07-11 22:39:17 +0900

    fix subsc

--- src/libs/wsClient.ts
+++ src/libs/wsClient.ts
@@ -12,18 +12,7 @@ export const getWsClient = ({ token, onConnected, onError }: Props) => {
   const wsClient = createClient({
     url,
     retryAttempts: 6,
-    keepAlive: 240000 + Math.random() * 45000,
-    shouldRetry: (error) => {
-      if (error instanceof CloseEvent && error.code === 4409) {
-        console.log("shouldRetry in error code 4409", error);
-
-        return true;
-      }
-
-      console.log("shouldRetry", error);
-
-      return false;
-    },
+    keepAlive: 180000 + Math.random() * 45000,
     connectionParams: () => {
       return {
         headers: {
@@ -34,8 +23,6 @@ export const getWsClient = ({ token, onConnected, onError }: Props) => {
     on: {
       connected: onConnected,
       error: onError,
-      ping: (receive) => console.log("ping", receive),
-      pong: (receive) => console.log("pong", receive),
     },
   });
 
