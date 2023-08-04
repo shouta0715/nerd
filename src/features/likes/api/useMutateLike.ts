de18401c1bc62a47722ab30d7edf18d356234347
shouta0715 <Myheroes0715@gmail.com>
2023-08-04 20:47:29 +0900

    fix likes gql

--- src/features/likes/api/useMutateLike.ts
+++ src/features/likes/api/useMutateLike.ts
@@ -1,16 +1,33 @@
+import { useMutation } from "@tanstack/react-query";
+import { deleteLikes, insertLikes } from "src/documents/likes";
 import {
-  useDeleteLikeMutation,
-  useInsertLikeMutation,
-} from "src/graphql/like/likeQuery.generated";
+  DeleteLikeMutation,
+  DeleteLikeMutationVariables,
+  InsertLikeMutation,
+  InsertLikeMutationVariables,
+} from "src/gql/graphql";
+import { Error } from "src/libs/error";
 import { client } from "src/libs/graphqlClient";
 
 export const useMutateLike = () => {
-  const insertLike = useInsertLikeMutation(client);
-  const deleteLike = useDeleteLikeMutation(client);
+  const insertLike = useMutation<
+    InsertLikeMutation,
+    Error,
+    InsertLikeMutationVariables
+  >({
+    mutationFn: (object) => client.request(insertLikes, object),
+  });
+  const deleteLike = useMutation<
+    DeleteLikeMutation,
+    Error,
+    DeleteLikeMutationVariables
+  >({
+    mutationFn: (object) => client.request(deleteLikes, object),
+  });
 
   return {
     insertLike,
     deleteLike,
-    isLoading: insertLike.isLoading || deleteLike.isLoading,
+    isLoading: insertLike.isPending || deleteLike.isPending,
   };
 };
