e25af62da92590a54dea700896b464e1dbf1b1a3
shouta0715 <Myheroes0715@gmail.com>
2023-08-07 12:55:10 +0900

    fix sp layout

--- src/components/Elements/Skeleton/live/header/index.tsx
+++ src/components/Elements/Skeleton/live/header/index.tsx
@@ -11,20 +11,20 @@ type Props = {
 export const HeaderSkeleton: FC<Props> = ({ title, number, sub_title }) => {
   return (
     <header className="flex flex-col px-4 lg:px-0">
-      <div className="flex w-full flex-1 flex-col items-center gap-1 lg:animate-gradient lg:rounded-t-2xl lg:bg-gradient-to-r lg:from-indigo-400  lg:to-orange-400 lg:bg-[length:400%] lg:px-4 lg:py-6 lg:text-white">
+      <div className="flex w-full flex-1 flex-col items-center gap-y-1 lg:animate-gradient lg:rounded-t-2xl lg:bg-gradient-to-r lg:from-indigo-400  lg:to-orange-400 lg:bg-[length:400%] lg:px-4 lg:py-6 lg:text-white">
         <Text
-          className="line-clamp-2 text-base font-bold md:text-lg "
+          className="line-clamp-1 text-sm font-bold md:line-clamp-2 md:text-lg "
           component="h4"
         >
           {title}
         </Text>
         {sub_title && (
           <Text className="flex" component="div">
-            <Text className="mr-2 text-sm md:text-base" component="p">
+            <Text className="mr-2 text-xs md:text-base" component="p">
               第{number}話
             </Text>
             <Text
-              className="line-clamp-2 flex-1 text-sm md:text-base"
+              className="line-clamp-1 flex-1 text-xs md:line-clamp-2 md:text-base"
               component="p"
             >
               {sub_title}
