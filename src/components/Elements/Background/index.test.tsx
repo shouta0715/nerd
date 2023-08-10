82420065065e336028a2e37b3010e43bbdbe9692
shouta0715 <Myheroes0715@gmail.com>
2023-08-10 15:47:27 +0900

    add components tests

---
+++ src/components/Elements/Background/index.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { Background } from "src/components/Elements/Background";
+
+describe("Components/Elements/Background", () => {
+  test("should render", () => {
+    render(<Background />);
+  });
+});
