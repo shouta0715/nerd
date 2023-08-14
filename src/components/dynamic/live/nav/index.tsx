7f103de30941238a3f3cfbc1a67df99615d279e0
shouta0715 <Myheroes0715@gmail.com>
2023-08-15 00:21:19 +0900

    fix nav size

--- src/components/dynamic/live/nav/index.tsx
+++ src/components/dynamic/live/nav/index.tsx
@@ -28,13 +28,13 @@ export const Nav: FC<Props> = ({ isChat, response, mode }) => {
         response === "lg" && "hidden flex-1 justify-around lg:flex",
 
         response === "sp"
-          ? "sticky top-0 z-20 justify-between border-b bg-white/80 px-2 before:h-5 before:w-5 before:content-[''] before:md:h-6 before:md:w-6 lg:hidden"
+          ? "sticky top-0 z-20 justify-between border-b bg-white/80 px-2  before:h-6 before:w-6 before:content-[''] lg:hidden"
           : ""
       )}
     >
       <Text
         className={clsx(
-          "inline-block rounded-none text-xs font-bold  text-indigo-600 transition-colors duration-300 md:text-base ",
+          "inline-block rounded-none text-sm font-bold  text-indigo-600 transition-colors duration-300 md:text-base ",
           mode === "up" ? " text-orange-600" : " text-indigo-600",
           !isChat && "opacity-50"
         )}
@@ -52,7 +52,7 @@ export const Nav: FC<Props> = ({ isChat, response, mode }) => {
       </Text>
       <Text
         className={clsx(
-          "inline-block  text-xs font-bold text-indigo-600 transition-colors duration-300 md:text-base ",
+          "inline-block  text-sm font-bold text-indigo-600 transition-colors duration-300 md:text-base ",
           isChat && "opacity-50",
           mode === "up" ? " text-orange-600" : " text-indigo-600"
         )}
@@ -72,7 +72,7 @@ export const Nav: FC<Props> = ({ isChat, response, mode }) => {
       {response === "sp" && (
         <button
           aria-label="エピソードメニューを開く"
-          className="h-5 w-5 transition-transform active:translate-y-0.5 md:h-6 md:w-6 lg:hidden"
+          className="h-6 w-6 transition-transform active:translate-y-0.5 lg:hidden"
           onClick={() => setIsNextOpen(!isNextOpen)}
         >
           <ListBulletIcon />
